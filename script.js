6752d00487d05b144f7dff3e8fdaf1df19de70ae
aniketkumar7 <aniketkumar639597@gmail.com>
2024-03-05 08:15:04 +0530

    Adding Loader

---
+++ script.js
@@ -0,0 +1,47 @@
+
+var tl = gsap.timeline()
+tl.from(".line h1", {
+    y:150,
+    duration: 0.6,
+    stagger: 0.25,
+    delay:0.5,
+}) 
+
+tl.from("#line1-part1", {
+    opacity:0,
+    onStart : function(){
+        var h5timer = document.querySelector("#line1-part1 h5")
+        var grow = 0;
+        setInterval(function(){
+            if(grow<100){
+                h5timer.innerHTML = grow++;
+            }
+            else{
+                h5timer.innerHTML = grow;
+            }
+        },35)
+    }
+})
+
+tl.to(".line h2",{
+    animationName: "anime",
+    opacity:1,
+})
+
+tl.to("#loader",{
+    opacity:0,
+    duration:0.4,
+    delay:3,
+})
+
+tl.from("#page1",{
+    delay: 0.2,
+    y:1600,
+    opacity:0,
+    duration:0.5,
+    ease: Power4
+})
+
+tl.to("#loader",{
+    display:"none",
+})
