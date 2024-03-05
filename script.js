e17e04dd812cc7f1a084cbbc45d2dfa7c82345e3
aniketkumar7 <aniketkumar639597@gmail.com>
2024-03-05 09:39:22 +0530

    page1 hero section done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+    
 var tl = gsap.timeline()
 tl.from(".line h1", {
     y:150,
@@ -31,7 +32,7 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
-    delay:3,
+    delay:2,
 })
 
 tl.from("#page1",{
@@ -45,3 +46,29 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none",
 })
+tl.from("#nav",{
+    opacity:0,
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger: 0.25,
+})
+}
+
+loadingAnimation()
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y,
+        })
+    })  
+}
+
+cursorAnimation()
+
+
+
+Shery.makeMagnet("#nav-part2 h4");
